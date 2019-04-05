e367e85ee42788970b31c7e2ec509b4004d699ac
Cogdof <chy4748@naver.com>
2019-04-05 17:26:33 +0900

    과제1 Additive cipher 코드 업로드 (이슈 #1)

--- Additive_Cipher/src/Main.java
+++ Additive_Cipher/src/Main.java
@@ -1,2 +1,41 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        String cipher = "NCJAEZRCLASJLYODEPRLYZRCLASJLCPEHZDTOPDZQLNZTY";
+
+        System.out.println((int)'A'+" "+(int)'Z');
+        System.out.println((char)65+" "+(char)90);
+
+
+        for(int key=0; key<27; key++) {
+            System.out.println(key +": key");
+            for(int i=0; i< cipher.length(); i++) {
+                int num=cipher.charAt(i);
+
+                num = Math.abs((num-key));
+                if(num<65) {//65 - 15 = 45
+                    num = 91-(65-num);
+                }
+                else {
+
+                }
+
+
+
+                char show = (char) (num);
+
+                System.out.print(show);
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+
+
+
+        for(int i='A'; i<='Z'; i++) {
+            System.out.println(i +" " +(char)i);
+        }
+
+    }
 }
