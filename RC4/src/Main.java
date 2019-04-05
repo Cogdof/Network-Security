8b62155ec009bb644e2259e22f897e0ddb3b0a32
Cogdof <chy4748@naver.com>
2019-04-05 18:04:05 +0900

    RC4 소스 작성 - 1 (이슈 #2)

--- RC4/src/Main.java
+++ RC4/src/Main.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        String key = input.nextLine();
+        System.out.println("key : "+key);
+
+
+        //입력받은 key <- 256 미만. 256 미만인경우 같은 key 내용이 반복해서 들어감.
+
+        //key 가 0으로만 채워져있을떄.
+
+        //key 길이가 256 일떄.
+
+    }
 
 }
