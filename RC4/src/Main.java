e806c191a431417290c9ea2b06251e73460c52a6
Cogdof <36148762+Cogdof@users.noreply.github.com>
2019-04-07 15:37:47 +0900

    RC4 소스 작성 - 2 구체적인 틀 수정  (이슈 #2)

--- RC4/src/Main.java
+++ RC4/src/Main.java
@@ -6,15 +6,37 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
+        //key 입력받기
         String key = input.nextLine();
         System.out.println("key : "+key);
 
+        int S[] = new int[256];
+        int K[] = new int[256];
+
+        for(int i=0; i<256; i++;){
+            S[i]= i;
+            K[i]=
+        }
+
+
+        //파일 입력받기
+
+
+
+        //평문 암호화
+
+
+
+
+        //암호문 복호화
+
+
 
         //입력받은 key <- 256 미만. 256 미만인경우 같은 key 내용이 반복해서 들어감.
-
         //key 가 0으로만 채워져있을떄.
-
         //key 길이가 256 일떄.
+
+
 
     }
 
