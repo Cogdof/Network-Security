39db884aa56d3cf67550c2a194b61f1f93da88da
Cogdof <chy4748@naver.com>
2019-04-08 17:57:48 +0900

    RC4 소스 작성 - 4  encryption 작성 중    (이슈 #2)

--- RC4/src/Main.java
+++ RC4/src/Main.java
@@ -3,56 +3,168 @@ import java.util.Scanner;
 
 public class Main {
 
+    //int s[] = new int[256];
+    static byte s[] = new byte[256];
+    static byte k[] = new byte[256];
+    static byte key[];
+
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
+        String keyStr="";
 
-        //key 입력받기
-        String keyStr = input.nextLine();
+
+        //=================     key 입력받기    =========================//
+        do {
+            System.out.println("Input the Key (1~256 Byte)");
+            keyStr = input.nextLine();
+        }
+        while(keyStr.length()< 1 || keyStr.length() > 256);     // Key 는 1 ~ 256 Byte 만 가능.
+
+        //입력받은 key <- 256 미만. 256 미만인경우 같은 key 내용이 반복해서 들어감.
+
         System.out.println("key : "+keyStr);
+        System.out.println(keyStr.length());
+        key = keyStr.getBytes();
 
-        byte s[] = new byte[256];
-        byte k[] = new byte[256];
 
-        byte key[]=keyStr.getBytes();
-        System.out.println(key.length);
+
+        //=================     Key 초기화 및 Key 스트림 생성  =========================//
+
+        for(int i=0; i<256; i++){
+            s[i]= (byte) i;
+            // s[i]=  i;
+            k[i]= key[i % key.length];
+            System.out.println( s[i] + " / "+ (char)k[i]);
+        }
+
+        int j=0;
+
+        for(int i=0; i<256; i++){
+            j =  ((j+s[i]+k[i])% 256);
+            swap(i,j);
+        }
+
+
+        //=================     File 읽어오기    =========================//
+
+        System.out.println("Choose mode : 1)Plain -> Cypher // 2) Cypher -> Plain 3) Exit");
+        int mode = input.nextInt();
+
+        switch (mode){
+
+            case 1:     //평문 암호화
+
+                try{
+                    File file = new File("src/plain_text.txt");
+                    FileReader fileReader = new FileReader(file);
+                    //  byte[] by=str.getBytes();
+                    // output.write(by);
+                    OutputStream output = new FileOutputStream("src/cipher_text.txt");
+                }
+                catch(Exception e){
+                    e.getStackTrace();
+                }
+
+
+            case 2:     //암호문 복호화
+
+                try{
+                    File file = new File("src/cipher_text.txt");
+                    FileReader fileReader = new FileReader(file);
+                    //  byte[] by=str.getBytes();
+                    // output.write(by);
+                    OutputStream output = new FileOutputStream("src/decrypt_text.txt");
+                }
+                catch(Exception e){
+                    e.getStackTrace();
+                }
+
+
+
+
+            case 3:     //시스템 종료.
+                System.out.println("System shut down.");
+                System.exit(0);;
+
+
+            default:
+                System.out.println("System shut down.");
+                System.exit(0);;
+        }
+
+
+
+
+
+
+        //=================     File 읽어오기    =========================//
 
         try{
-            File file = new File("src/plaintext.txt");
+            File file = new File("src/plain_text.txt");
             FileReader fileReader = new FileReader(file);
-          //  byte[] by=str.getBytes();
-          // output.write(by);
-            OutputStream output = new FileOutputStream("src/ciphertext.txt");
+            BufferedReader buffer = new BufferedReader(fileReader);
+            OutputStream output = new FileOutputStream("src/cipher_text.txt");
+            //String line = buffer.readLine();
+            String line="";
+
+            do{
+              line = buffer.readLine();
+              byte[] cipher = line.getBytes();
+              byte[] result = encryption(cipher);
+
+              //hexadecimal 로 text 저장. 필요!
+              output.write(result);
+            }while(!line.equals(null));
+
+            buffer.close();
+            output.close();
         }
         catch(Exception e){
             e.getStackTrace();
         }
 
-        for(int i=0; i<256; i++){
-            s[i]= (byte) i;
-            k[i]= key[i%key.length];
-            System.out.println(s[i] + " / "+ (char)k[i]);
+
+
+
+
+
+
+
+
+        //=================     File 저장하기    =========================//
+
+
+
+
+
+    }
+
+    public static void swap(int i, int j){
+        byte temp = s[i];
+        s[i] = s[j];
+        s[j] = s[i];
+    }
+
+    public static byte[] encryption(byte [] bytes){
+        byte[] result;
+
+        return result;
+    }
+
+    public static byte[] decryption(byte [] cipher_line){
+        final byte[] ciphertext = new byte[cipher_line.length];
+        int i = 0, j = 0, k, t;
+        byte tmp;
+        for (int counter = 0; counter < cipher_line.length; counter++) {
+            i = (i + 1) & 0xFF;
+            j = (j + s[i]) & 0xFF;
+            swap(i, j);
+            t = (s[i] + s[j]) & 0xFF;
+            k = s[t];
+            ciphertext[counter] = (byte) (cipher_line[counter] ^ k);
         }
-
-
-        //파일 입력받기
-
-
-
-        //평문 암호화
-
-
-
-
-        //암호문 복호화
-
-
-
-        //입력받은 key <- 256 미만. 256 미만인경우 같은 key 내용이 반복해서 들어감.
-        //key 가 0으로만 채워져있을떄.
-        //key 길이가 256 일떄.
-
-
+        return ciphertext;
 
     }
 
@@ -80,5 +192,7 @@ public class Main {
         }
 
     }
+
+
 
 }
